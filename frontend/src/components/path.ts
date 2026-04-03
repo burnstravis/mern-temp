14cc00cde7756992c32bfec0d63b6af0b902e267
Travis Burns <burns.travis.j@gmail.com>
2026-04-03 18:04:37 -0400

    fix domain path

--- frontend/src/components/path.ts
+++ frontend/src/components/path.ts
@@ -2,9 +2,11 @@ const app_name = 'localhost:3000';
 
 export const buildPath = (route: string): string => {
 
-    if (import.meta.env.MODE !== 'development') {
-        return 'https://' + app_name + '/' + route;
-    } else {
-        return 'http://localhost:3000/' + route;
-    }
+    // if (import.meta.env.MODE !== 'development') {
+    //     return 'https://' + app_name + '/' + route;
+    // } else {
+    //     return 'http://localhost:3000/' + route;
+    // }
+
+    return 'https://largeproject.nathanfoss.me';
 };
