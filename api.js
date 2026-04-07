b7543815e78da40e0349ebbe2510f1d1d167b552
Eduard Uy <ecuy0705@gmail.com>
2026-04-06 20:02:30 -0400

    Fixing Sending to Email

--- api.js
+++ api.js
@@ -6,7 +6,9 @@ const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 
 const mailer = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
