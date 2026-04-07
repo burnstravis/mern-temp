8e8ffea6aae7e2730d661367b7ba4d320c89a726
Eduard Uy <ecuy0705@gmail.com>
2026-04-06 20:10:37 -0400

    Update api.js

--- api.js
+++ api.js
@@ -7,8 +7,8 @@ const crypto = require('crypto');
 
 const mailer = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
