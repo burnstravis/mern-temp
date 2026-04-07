b0691bc2cae006af96414bc73d70976626d470fa
burnstravis <burns.travis.j@gmail.com>
2026-04-07 17:29:36 -0400

    forgot password fix

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -66,7 +66,7 @@ function Login()
     }
 
     function goToForgotPassword(): void {
-        navigate('forgotPassword');
+        navigate('/forgotPassword');
     }
     function handleSetLoginName( e: any ) : void
     {
