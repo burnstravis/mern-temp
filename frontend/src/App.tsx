935c97b64ea2b1142a4daaa0bbb560283b724215
Raeraelee <raeleemail@gmail.com>
2026-04-08 00:30:04 -0400

    NotificationsPage
    Added notifications page to frontend. Needs 3 APIs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import LoginPage from './pages/LoginPage.tsx';
 import ForgotPasswordPage from './pages/ForgotPasswordPage.tsx';
 import CardPage from './pages/CardPage.tsx';
 import RegisterPage from "./pages/RegisterPage.tsx";
+import NotificationPage from "./pages/NotificationPage.tsx";
 import LandingPage from "./pages/LandingPage.tsx";
 import ResetPasswordPage from "./pages/ResetPasswordPage.tsx";
 import { Navigate, Outlet } from 'react-router-dom';
@@ -23,6 +24,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
             <Route path="/resetPassword" element={<ResetPasswordPage />} />
+            <Route path="/notification" element={<NotificationPage />} />
             <Route element={<ProtectedRoute />}>
                 <Route path="/cards" element={<CardPage />} />
             </Route>
---
+++ frontend/src/pages/NotificationPage.tsx
@@ -0,0 +1,14 @@
+import Notifications from '../components/Notifications';
+
+
+const NotificationPage = () => {
+  return (
+    <div id="registerWrapper" className="wrapper">
+      <div id="registerContainer" className="container">
+        <Notifications />
+      </div>
+    </div>
+  );
+};
+
+export default NotificationPage;
